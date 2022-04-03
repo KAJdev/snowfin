6a18cb60bc7616a9bcaa85b47b38b70313c98337
KAJ7 <zekewotring@gmail.com>
2022-04-03 11:22:44 -0800

    better slash_option example

--- examples/option_example.py
+++ examples/option_example.py
@@ -1,18 +1,15 @@
 import asyncio
 import snowfin
 from snowfin.client import AutoDefer
-from snowfin.decorators import SlashOption
+from snowfin.decorators import slash_command, slash_option
 from snowfin.enums import OptionType
 from snowfin.models import Interaction
 from snowfin.response import MessageResponse
 
 bot = snowfin.Client('public_key', 'application_id', 'token', auto_defer=AutoDefer(enabled=True, timeout=0, ephemeral=True))
 
-@snowfin.slash_command(name="hello",
-    options=[
-        SlashOption(name="option", description="this is a test option", type=OptionType.BOOLEAN)
-    ]
-)
+@slash_command("hello")
+@slash_option(name="option", description="this is a test option", type=OptionType.BOOLEAN)
 async def hello(context: Interaction, option: bool = None):
     """
     Slash command docstrings can be used for descriptions
